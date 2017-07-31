956ceff9c20ab039afb7da5ec8fdadecff9b8929
framgia.nguyen.van.quang <framgia@framgia0147-lt.framgia.com>
2017-07-31 09:11:04 +0700

    send login api

---
+++ app/src/main/java/com/framgia/feastival/data/source/LoginDataSource.java
@@ -0,0 +1,7 @@
+package com.framgia.feastival.data.source;
+
+/**
+ * Created by framgia on 31/07/2017.
+ */
+public interface LoginDataSource {
+}
---
+++ app/src/main/java/com/framgia/feastival/data/source/remote/LoginRemoteDataSource.java
@@ -0,0 +1,7 @@
+package com.framgia.feastival.data.source.remote;
+
+/**
+ * Created by framgia on 31/07/2017.
+ */
+public class LoginRemoteDataSource {
+}
